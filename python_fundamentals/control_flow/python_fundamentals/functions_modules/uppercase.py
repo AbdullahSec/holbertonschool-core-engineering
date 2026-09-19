970de245584db1787e72a04399b12b6c8b87e4c0
AbdullahSec <Talonkali@outlook.com>
2026-09-19 21:33:39 +0300

    Complete functions and modules tasks 0 through 7

---
+++ python_fundamentals/control_flow/python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    res = ""
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            res += chr(ord(c) - 32)
+        else:
+            res += c
+    print("{}".format(res))
